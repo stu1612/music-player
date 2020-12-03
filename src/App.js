53547b928804759fb2759d76120f5beaed9c9099
Stu Bolderson <stu.bolderson@aol.com>
2020-12-03 18:44:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useRef } from 'react';
+// Components
+import './styles/app.scss';
+import Player from './components/Player';
+import Song from './components/Song';
+import Library from './components/Library';
+// Import data
+import data from './data';
+
+function App() {
+  // Ref
+  const audioRef = useRef(null);
+  // state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
+
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
+  };
+
+  return (
+    <div className='App'>
+      <Song currentSong={currentSong} />
+      <Player
+        audioRef={audioRef}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+      />
+      <Library
+        audioRef={audioRef}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LibrarySong.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const LibrarySong = ({
+  songs,
+  song,
+  setCurrentSong,
+  id,
+  audioRef,
+  isPlaying,
+}) => {
+  const songSelectHandler = () => {
+    const selectedSong = songs.filter((state) => state.id === id);
+    setCurrentSong(selectedSong[0]);
+    // check if song is playing
+    if (isPlaying) {
+      const playPromise = audioRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.then((audio) => {
+          audioRef.current.play();
+        });
+      }
+    }
+  };
+  return (
+    <div onClick={songSelectHandler} className='library-song'>
+      <img src={song.cover} alt={song.name} />
+      <div className='song-description'>
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
